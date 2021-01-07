33d95f6efc06b808a60225cd65bf5da67cf4be3b
Nikhil Upadhyay <nikhil9690@gmail.com>
2021-01-07 12:47:03 +0530

    Bug fixed in notices screen

--- app/screens/NoticesScreen.js
+++ app/screens/NoticesScreen.js
@@ -5,7 +5,6 @@ import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
 import Screen from "../components/Screen";
-import Button from "../components/Button";
 import Text from "../components/Text";
 
 function NoticesScreen({ navigation }) {
@@ -42,7 +41,7 @@ function NoticesScreen({ navigation }) {
         renderItem={({ item }) => (
           <Card
             title={item.title}
-            subTitle={item.description}
+            subTitle={item.venue}
             onPress={() => navigation.navigate(routes.NOTICES, item)}
           />
         )}
